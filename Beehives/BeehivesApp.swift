37320520eded1040da0254d9436384fd78557ba3
ygeras <86482316+Yus312@users.noreply.github.com>
2023-04-06 21:54:34 +0300

    Initial Commit

---
+++ Beehives/BeehivesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeehivesApp.swift
+//  Beehives
+//
+//  Created by Yuri Gerasimchuk on 06.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BeehivesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
